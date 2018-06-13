29e198c102ca0d4763720f596825eec9dc528815
Thiago Ferrão <thiago.ferrao91@gmail.com>
2018-06-13 11:17:19 -0300

    creates the BaseCallback interface to be used between the interactor and the service layers

---
+++ app/src/main/java/thiagocruz/weatherforall/services/BaseCallback.kt
@@ -0,0 +1,11 @@
+package thiagocruz.weatherforall.services
+
+import retrofit2.Call
+import retrofit2.Response
+
+interface BaseCallback<T> {
+    fun onResponse(call: Call<T>?, response: Response<T>?)
+    fun onInternalError(call: Call<T>?, response: Response<T>?)
+    fun onConnectionError(call: Call<T>?, t: Throwable?)
+    fun onGeneralError(call: Call<T>?, t: Throwable?)
+}
