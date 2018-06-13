639beb36dcc1c26cc9ff407c78dc9c52ba7fba1f
Thiago Cruz <thiago.ferrao91@gmail.com>
2018-06-13 05:48:07 -0300

    creates the LocationUtil class that will calculate the forecast perimeter based on the user current location

---
+++ app/src/main/java/thiagocruz/weatherforall/utils/LocationUtil.kt
@@ -0,0 +1,35 @@
+package thiagocruz.weatherforall.utils
+
+import android.location.Location
+import kotlin.math.PI
+import kotlin.math.cos
+
+// Logic Found On The Link Below
+// https://stackoverflow.com/questions/7477003/calculating-new-longitude-latitude-from-old-n-meters?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+
+object LocationUtil {
+
+    private const val DEFAULT_PERIMETER_RADIUS: Double = 50.0
+    private const val EARTH_RADIUS: Double = 6371.0
+    private const val HALF_TURN: Double = 180.0
+
+    fun getDefaultPerimeter(currentLocation: Location): String {
+        val currentLatitude = currentLocation.latitude
+        val currentLongitude = currentLocation.longitude
+
+        val topLat = getNewLatitudeFrom(currentLatitude, DEFAULT_PERIMETER_RADIUS)
+        val bottomLat = getNewLatitudeFrom(currentLatitude, -DEFAULT_PERIMETER_RADIUS)
+        val leftLong = getNewLongitudeFrom(currentLatitude, currentLongitude, DEFAULT_PERIMETER_RADIUS)
+        val rightLong = getNewLongitudeFrom(currentLatitude, currentLongitude, -DEFAULT_PERIMETER_RADIUS)
+
+        return doubleArrayOf(leftLong, bottomLat, rightLong, topLat).joinToString(",")
+    }
+
+    fun getNewLatitudeFrom(currentLatitude: Double, addKilometers: Double): Double {
+        return currentLatitude + (addKilometers / EARTH_RADIUS) * (HALF_TURN / PI)
+    }
+
+    fun getNewLongitudeFrom(currentLatitude: Double, currentLongitude: Double, addKilometers: Double): Double {
+        return currentLongitude + (addKilometers / EARTH_RADIUS) * (HALF_TURN / PI) / cos(currentLatitude * PI / HALF_TURN)
+    }
+}
