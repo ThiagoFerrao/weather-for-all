2ebe0d87db6ca334b20287e94ef242e6012c785b
Thiago Cruz <thiago.ferrao91@gmail.com>
2018-06-12 22:33:56 -0300

    creates the SplashActivity class that will be use as the launch activity

---
+++ app/src/main/java/thiagocruz/weatherforall/ui/activities/SplashActivity.kt
@@ -0,0 +1,14 @@
+package thiagocruz.weatherforall.ui.activities
+
+import android.os.Bundle
+import android.app.Activity
+import thiagocruz.weatherforall.R
+
+class SplashActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+
+}
