69f02f9a5a1366ba945d202fd444899997b1505a
Thiago Cruz <thiago.ferrao91@gmail.com>
2018-06-12 23:05:45 -0300

    adds a daley to the SplashActivity before calling the MainActivity

--- app/src/main/java/thiagocruz/weatherforall/ui/activities/SplashActivity.kt
+++ app/src/main/java/thiagocruz/weatherforall/ui/activities/SplashActivity.kt
@@ -2,13 +2,21 @@ package thiagocruz.weatherforall.ui.activities
 
 import android.os.Bundle
 import android.app.Activity
+import android.content.Intent
 import thiagocruz.weatherforall.R
+import thiagocruz.weatherforall.utils.DelayUtil
 
 class SplashActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
-    }
 
+        setContentView(R.layout.activity_splash)
+
+        DelayUtil.standardDelay {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
 }
