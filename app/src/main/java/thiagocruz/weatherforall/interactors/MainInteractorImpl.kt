38e30625b4b72919e2663a3f76eea0536562c067
Thiago Ferrão <thiago.ferrao91@gmail.com>
2018-06-13 11:18:12 -0300

    adds the basecallback as a parameter to the findWeatherForecast method

--- app/src/main/java/thiagocruz/weatherforall/interactors/MainInteractorImpl.kt
+++ app/src/main/java/thiagocruz/weatherforall/interactors/MainInteractorImpl.kt
@@ -1,6 +1,10 @@
 package thiagocruz.weatherforall.interactors
 
 import android.location.Location
+import retrofit2.Call
+import retrofit2.Response
+import thiagocruz.weatherforall.entities.WeatherApiResponse
+import thiagocruz.weatherforall.services.BaseCallback
 import thiagocruz.weatherforall.services.WeatherForecastService
 import thiagocruz.weatherforall.utils.LocationConverterUtil
 
@@ -9,6 +13,23 @@ class MainInteractorImpl : MainInteractor {
     override fun findWeatherForecast(location: Location, listener: MainInteractor.WeatherForecastListener) {
         val forecastPerimeter = LocationConverterUtil.getDefaultPerimeter(location)
 
-        WeatherForecastService.getWeatherForecast(forecastPerimeter + ",10", "metric")
+        WeatherForecastService.getWeatherForecast("$forecastPerimeter,20", "metric",
+                object : BaseCallback<WeatherApiResponse> {
+                    override fun onResponse(call: Call<WeatherApiResponse>?, response: Response<WeatherApiResponse>?) {
+
+                    }
+
+                    override fun onInternalError(call: Call<WeatherApiResponse>?, response: Response<WeatherApiResponse>?) {
+
+                    }
+
+                    override fun onConnectionError(call: Call<WeatherApiResponse>?, t: Throwable?) {
+
+                    }
+
+                    override fun onGeneralError(call: Call<WeatherApiResponse>?, t: Throwable?) {
+
+                    }
+                })
     }
 }
--- app/src/main/java/thiagocruz/weatherforall/services/WeatherForecastService.kt
+++ app/src/main/java/thiagocruz/weatherforall/services/WeatherForecastService.kt
@@ -7,22 +7,25 @@ import thiagocruz.weatherforall.BuildConfig
 import thiagocruz.weatherforall.Constant
 import thiagocruz.weatherforall.entities.WeatherApiResponse
 import thiagocruz.weatherforall.utils.RetrofitUtil
+import thiagocruz.weatherforall.utils.ServiceErrorUtil
 
 object WeatherForecastService {
 
-    fun getWeatherForecast(perimeter: String, temperatureUnit: String) {
+    fun getWeatherForecast(perimeter: String, temperatureUnit: String, callback: BaseCallback<WeatherApiResponse>) {
         val weatherForecastService = RetrofitUtil.buildRequest().create(WeatherForecastRetrofitService::class.java)
         val call = weatherForecastService.forecast(perimeter, temperatureUnit, Constant.AppLanguage.PORTUGUESE, BuildConfig.OPEN_WHEATER_MAP_API_KEY)
 
         call.enqueue(object : Callback<WeatherApiResponse> {
             override fun onResponse(call: Call<WeatherApiResponse>?, response: Response<WeatherApiResponse>?) {
-                val aux = response?.body()
-                print(aux)
+                if (ServiceErrorUtil.hasInternalError(response)) return callback.onInternalError(call, response)
+
+                callback.onResponse(call, response)
             }
 
             override fun onFailure(call: Call<WeatherApiResponse>?, t: Throwable?) {
-                val aux = call.toString()
-                print(aux)
+                if (ServiceErrorUtil.hasConnectionError(t)) return callback.onConnectionError(call, t)
+
+                callback.onGeneralError(call, t)
             }
         })
     }
