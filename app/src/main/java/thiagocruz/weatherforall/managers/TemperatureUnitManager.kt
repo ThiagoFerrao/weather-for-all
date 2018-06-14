69d65126707dbcf7992a1d2c8826c8612f09c0ba
Thiago Cruz <thiago.ferrao91@gmail.com>
2018-06-14 01:26:29 -0300

    creates the TemperatureUnitManager to manage the change of temperature units

---
+++ app/src/main/java/thiagocruz/weatherforall/managers/TemperatureUnitManager.kt
@@ -0,0 +1,47 @@
+package thiagocruz.weatherforall.managers
+
+import android.content.Context
+import android.view.MenuItem
+import thiagocruz.weatherforall.Constant
+import thiagocruz.weatherforall.R
+
+object TemperatureUnitManager {
+
+    fun updateTemperatureUnit(context: Context?, menuItem: MenuItem?) {
+        val preferences = context?.getSharedPreferences(Constant.SharedPreferences.DEFAULT, Context.MODE_PRIVATE)
+        val editor = preferences?.edit()
+
+        val currentTemperatureUnit = context?.let { getCurrentTemperatureUnit(it) }
+
+        if (isTemperatureUnitCelsius(currentTemperatureUnit)) {
+            editor?.putString(Constant.SharedPreferences.KEY_TEMPERATURE_UNIT, Constant.TemperatureUnit.FAHRENHEIT)
+            menuItem?.icon = context?.getDrawable(R.drawable.ic_temp_fahrenheit)
+
+        } else {
+            editor?.putString(Constant.SharedPreferences.KEY_TEMPERATURE_UNIT, Constant.TemperatureUnit.CELSIUS)
+            menuItem?.icon = context?.getDrawable(R.drawable.ic_temp_celsius)
+        }
+
+        editor?.apply()
+    }
+
+    fun getCurrentTemperatureUnit(context: Context): String {
+        val preferences = context.getSharedPreferences(Constant.SharedPreferences.DEFAULT, Context.MODE_PRIVATE)
+        return preferences.getString(Constant.SharedPreferences.KEY_TEMPERATURE_UNIT, Constant.TemperatureUnit.CELSIUS)
+    }
+
+    fun setCurrentMenuItemIcon(context: Context, menuItem: MenuItem?) {
+        if (isTemperatureUnitCelsius(getCurrentTemperatureUnit(context))) {
+            menuItem?.icon = context.getDrawable(R.drawable.ic_temp_celsius)
+            return
+        }
+
+        menuItem?.icon = context.getDrawable(R.drawable.ic_temp_fahrenheit)
+    }
+
+    private fun isTemperatureUnitCelsius(tempUnit: String?): Boolean {
+        if (tempUnit == null) return true
+
+        return tempUnit == Constant.TemperatureUnit.CELSIUS
+    }
+}
