70fc911bbf5f35f7574572d71c64d6dbb3bd4a52
Thiago Cruz <thiago.ferrao91@gmail.com>
2018-06-14 02:33:45 -0300

    sorts city forecast list based on user's location

--- app/src/main/java/thiagocruz/weatherforall/interactors/MainInteractorImpl.kt
+++ app/src/main/java/thiagocruz/weatherforall/interactors/MainInteractorImpl.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.location.Location
 import retrofit2.Call
 import retrofit2.Response
+import thiagocruz.weatherforall.Constant
+import thiagocruz.weatherforall.entities.CityForecast
 import thiagocruz.weatherforall.entities.WeatherApiResponse
 import thiagocruz.weatherforall.managers.TemperatureUnitManager
 import thiagocruz.weatherforall.services.BaseCallback
@@ -19,11 +21,12 @@ class MainInteractorImpl : MainInteractor {
         WeatherForecastService.getWeatherForecast(forecastPerimeter, temperatureUnit,
                 object : BaseCallback<WeatherApiResponse> {
                     override fun onResponse(call: Call<WeatherApiResponse>?, response: Response<WeatherApiResponse>?) {
-                        val result = response?.body()?.cityForecastList ?: return listener.errorWhileFetchingWeatherForecast()
+                        val result = response?.body()?.cityForecastList
+                                ?: return listener.errorWhileFetchingWeatherForecast()
 
                         if (result.isEmpty()) return listener.emptyWeatherForecast()
 
-                        listener.foundWeatherForecast(result)
+                        sortCityForecastListByUserDistance(result, context, listener)
                     }
 
                     override fun onInternalError(call: Call<WeatherApiResponse>?, response: Response<WeatherApiResponse>?) {
@@ -38,5 +41,17 @@ class MainInteractorImpl : MainInteractor {
                         listener.errorWhileFetchingWeatherForecast()
                     }
                 })
+    }
+
+    private fun sortCityForecastListByUserDistance(list: List<CityForecast>, context: Context, listener: MainInteractor.WeatherForecastListener) {
+        val preferences = context.getSharedPreferences(Constant.SharedPreferences.DEFAULT, Context.MODE_PRIVATE)
+        val userLatitude = preferences.getString(Constant.SharedPreferences.KEY_USER_LOCATION_LATITUDE, null)?.toDouble()
+        val userLongitude = preferences.getString(Constant.SharedPreferences.KEY_USER_LOCATION_LONGITUDE, null)?.toDouble()
+
+        if (userLatitude == null || userLongitude == null) return listener.foundWeatherForecast(list)
+
+        val sortedList = list.sortedWith(compareBy { it.coordinate.distanceToLatLngInKm(userLatitude, userLongitude) })
+
+        listener.foundWeatherForecast(sortedList)
     }
 }
