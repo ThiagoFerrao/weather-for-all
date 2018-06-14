74c55727b50492b44d88565b094c4a3a2eb4025c
Thiago Cruz <thiago.ferrao91@gmail.com>
2018-06-14 17:26:48 -0300

    adds some customization to the map marks

--- app/src/main/java/thiagocruz/weatherforall/ui/activities/MapActivity.kt
+++ app/src/main/java/thiagocruz/weatherforall/ui/activities/MapActivity.kt
@@ -2,18 +2,22 @@ package thiagocruz.weatherforall.ui.activities
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.support.v7.widget.Toolbar
 import android.view.Menu
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
+import com.squareup.picasso.Picasso
+import com.squareup.picasso.Target
 import kotlinx.android.synthetic.main.activity_main.*
 import thiagocruz.weatherforall.Constant
 import thiagocruz.weatherforall.R
 import thiagocruz.weatherforall.managers.TemperatureUnitManager
+import java.lang.Exception
 
 class MapActivity : BaseActivity(), OnMapReadyCallback {
 
@@ -73,7 +77,39 @@ class MapActivity : BaseActivity(), OnMapReadyCallback {
                 val cityName = cityForecast.name
                 val cityLatLng = cityForecast.coordinate.getCityLatLng()
 
-                mMap?.addMarker(MarkerOptions().position(cityLatLng).title(cityName))
+                val tempUnit = TemperatureUnitManager.getCurrentTemperatureUnit(this)
+                val degreesPrefix = if (TemperatureUnitManager.isTemperatureUnitCelsius(tempUnit)) {
+                    "°C"
+                } else {
+                    "°F"
+                }
+                val cityMinTemp = String.format("Min: %.1f$degreesPrefix", cityForecast.temperature.minimumTemp)
+                val cityMaxTemp = String.format("Max: %.1f$degreesPrefix", cityForecast.temperature.maximumTemp)
+
+                val markerOptions = MarkerOptions()
+                        .position(cityLatLng)
+                        .title(cityName)
+                        .snippet("$cityMaxTemp  $cityMinTemp")
+
+                Picasso.get()
+                        .load(cityForecast.weather.first().getWeatherImageUrl())
+                        .placeholder(R.drawable.logo_splash)
+                        .error(R.drawable.logo_splash)
+                        .into(object : Target {
+                            override fun onPrepareLoad(placeHolderDrawable: Drawable?) {
+
+                            }
+
+                            override fun onBitmapFailed(e: Exception?, errorDrawable: Drawable?) {
+                                mMap?.addMarker(markerOptions)
+                            }
+
+                            override fun onBitmapLoaded(bitmap: Bitmap?, from: Picasso.LoadedFrom?) {
+                                val weatherIcon = Bitmap.createScaledBitmap(bitmap, 150, 150, false)
+                                markerOptions.icon(BitmapDescriptorFactory.fromBitmap(weatherIcon))
+                                mMap?.addMarker(markerOptions)
+                            }
+                        })
             }
         }
     }
@@ -91,5 +127,9 @@ class MapActivity : BaseActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
         mMap?.uiSettings?.isMapToolbarEnabled = false
+        mMap?.setOnMarkerClickListener { marker: Marker? ->
+            marker?.showInfoWindow()
+            true
+        }
     }
 }
