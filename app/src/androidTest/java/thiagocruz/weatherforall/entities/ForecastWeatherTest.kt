311fa7da1aecc4e8d9a2f5027b15017316d29625
Thiago Cruz <thiago.ferrao91@gmail.com>
2018-06-19 04:03:10 -0300

    adds the testGetWeatherImageUrlIsLoaded to the ForecastWeatherTest class

--- app/src/androidTest/java/thiagocruz/weatherforall/entities/ForecastWeatherTest.kt
+++ app/src/androidTest/java/thiagocruz/weatherforall/entities/ForecastWeatherTest.kt
@@ -1,8 +1,17 @@
 package thiagocruz.weatherforall.entities
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import android.os.Handler
+import android.support.annotation.UiThread
 import android.webkit.URLUtil
+import com.squareup.picasso.Picasso
+import com.squareup.picasso.Target
 import org.junit.Before
 import org.junit.Test
+import java.lang.Exception
+import android.os.Looper
+
 
 class ForecastWeatherTest {
 
@@ -21,5 +30,27 @@ class ForecastWeatherTest {
         val weatherIcon = testForecastWeather?.getWeatherImageUrl()
 
         assert(URLUtil.isValidUrl(weatherIcon))
+    }
+
+    @Test
+    @UiThread
+    fun testGetWeatherImageUrlIsLoaded() {
+        val weatherIcon = testForecastWeather?.getWeatherImageUrl()
+
+        Handler(Looper.getMainLooper()).post {
+            Picasso.get()
+                    .load(weatherIcon)
+                    .into(object : Target {
+                        override fun onPrepareLoad(placeHolderDrawable: Drawable?) {
+                        }
+
+                        override fun onBitmapFailed(e: Exception?, errorDrawable: Drawable?) {
+                            AssertionError("testGetWeatherImageUrlIsLoaded - onBitmapFailed")
+                        }
+
+                        override fun onBitmapLoaded(bitmap: Bitmap?, from: Picasso.LoadedFrom?) {
+                        }
+                    })
+        }
     }
 }
